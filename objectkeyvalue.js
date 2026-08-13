e600a1e22f19e83e4e76c3ada99f3ed3056baf38
Sanskriti Pandey <sanskritipandey2692@gmail.com>
2026-08-13 14:21:15 +0530

    object file created

---
+++ objectkeyvalue.js
@@ -0,0 +1,22 @@
+// Using curly braces {} - Most common way
+let student = {
+  name: "John",
+  age: 30,
+  cgpa:9.8,
+  isPass:true
+  
+};
+
+// // Dot notation (preferred)
+// console.log(student);  // "John"
+
+// // Bracket notation (useful for dynamic keys)
+// console.log(student["age"]);  // 30
+// console.log(student.name);//john
+// student.name="John pandey";
+// console.log(student);
+
+let marks=[34,56,78,89];
+console.log(marks[0]);
+
+
