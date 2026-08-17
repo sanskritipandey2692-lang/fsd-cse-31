feb8112bbcbd5c8cb409b2c8f78a22a2a757e28c
Sanskriti Pandey <sanskritipandey2692@gmail.com>
2026-08-17 11:17:03 +0530

    length found

---
+++ length.js
@@ -0,0 +1,14 @@
+let a="Sanskriti Pandey"
+let count=0
+// 1st method
+// for(let i=0;i<a.length;i++){
+//     count++
+// }
+// console.log("length of the string is",count)
+// 2nd method
+while(a[count]!=undefined){
+    count++
+}
+console.log("length of the string is",count)
+// 3rd method
+console.log("length of the string is",a.length)
---
+++ template.js
@@ -0,0 +1,6 @@
+let obj={name : "Mango",
+        price:10000000000   
+}
+console.log("the cost of", obj.name, "is", "$",obj.price);
+console.log(`the cost of ${obj.name} is ${obj.price}`);
+console.log(`the sum of ${1+2+3+4}`);
